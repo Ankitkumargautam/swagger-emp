94d5da06b3b2a3a4d095c54eb0d0e6d6820eb6d3
Ankitkumargautam <ankitogn4@gmail.com>
2022-07-25 01:20:25 +0530

    create doc -error

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # swagger-emp
+
+1. Swagger implementation with node application.
+
+2. Swagger create docs -error
--- routes/emp.js
+++ routes/emp.js
@@ -8,7 +8,78 @@ import {
 
 const router = express.Router();
 
+//Defining the schema
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     emp:
+ *       type: object
+ *       required:
+ *         - empno
+ *         - ename
+ *         - job
+ *         - hiredate
+ *         - salary
+ *       properties:
+ *         empno:
+ *           type: number
+ *           description: The auto-generated id of the book
+ *         ename:
+ *           type: string
+ *           description: The emp ename
+ *         job:
+ *           type: string
+ *           description: The emp job name
+ *         hiredate:
+ *           type: string
+ *           description: The emp job name
+ *         salary:
+ *           type: string
+ *           description: The emp job name
+ *
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
+ */
+
+//This is the starting tag that will come on the top
+
+/**
+ * @swagger
+ * tags:
+ *   name: Employee
+ *   description: Employee managing API
+ */
+
 router.get('/', getEmpData);
+
+/**
+ * @swagger
+ * /emp:
+ *   post:
+ *     summary: Create a new employee
+ *     tags: [Employee]
+ *     basePath: '../'
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: 'models/emp.js'
+ *     responses:
+ *       200:
+ *         description: Employee was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: 'models/emp.js'
+ *       500:
+ *         description: Some server error
+ */
+
 router.post('/', createEmpData);
 router.patch('/:id', updateEmpData);
 router.delete('/:id', deleteEmpData);
