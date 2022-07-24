66a58ce8506015930b279fce4d88e9fce8b80a4c
Ankitkumargautam <ankitogn4@gmail.com>
2022-07-25 00:44:16 +0530

    swagger implementation

---
+++ index.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import empRoutes from './routes/emp.js';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Employee API',
+      version: '1.0.0',
+      description: 'A simple Employee API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+
+//routes
+app.use('/emp', empRoutes);
+
+const port = process.env.PORT;
+const CONNECTION_URL = process.env.DB;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(port, () =>
+      console.log(`Server Running on Port: http://localhost:${port}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
